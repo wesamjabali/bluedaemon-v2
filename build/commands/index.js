0cdc0fd72db98f52f853243123977c85f0e09168
Wesam Jabali <wesamjabali@icloud.com>
2021-09-02 02:28:55 -0500

    Delete build directory

--- build/commands/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PingCommand = exports.commands = void 0;
-const ping_command_1 = require("./ping.command");
-Object.defineProperty(exports, "PingCommand", { enumerable: true, get: function () { return ping_command_1.PingCommand; } });
-const commands = [new ping_command_1.PingCommand()];
-exports.commands = commands;
