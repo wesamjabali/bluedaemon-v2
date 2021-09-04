0f38bb9b829152a9f861c790fff71ae266c830c9
Wesam Jabali <wesamjabali@icloud.com>
2021-09-04 01:05:01 -0500

    Add embed and button on source command

--- src/commands/source.command.ts
+++ src/commands/source.command.ts
@@ -1,4 +1,11 @@
-import { CommandInteraction } from "discord.js";
+import { ButtonAction } from "@/buttons/ButtonAction.service";
+import {
+  CommandInteraction,
+  Formatters,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+} from "discord.js";
 import { ICommand } from "./command.interface";
 
 export class SourceCommand implements ICommand {
@@ -7,6 +14,25 @@ export class SourceCommand implements ICommand {
   default_permission = true;
 
   public async execute(interaction: CommandInteraction): Promise<void> {
-    await interaction.reply("https://github.com/wesamjabali/bluedaemon-v2");
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle("Check me out on GitHub!")
+          .setURL("https://github.com/wesamjabali/bluedaemon-v2")
+          .setImage(
+            "https://github.com/wesamjabali/BlueDaemon/blob/main/assets/banner.png?raw=true"
+          )
+          .setFields({ name: "Author", value: "Wesam Jabali" })
+          .setColor("BLUE"),
+      ],
+      components: [
+        new MessageActionRow().addComponents([
+          new MessageButton()
+            .setLabel("GitHub")
+            .setURL("https://github.com/wesamjabali/bluedaemon-v2")
+            .setStyle("LINK"),
+        ]),
+      ],
+    });
   }
 }
