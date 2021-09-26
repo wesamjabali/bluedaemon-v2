16c019542b87a48c30c264354cc1bc74ff11db98
Wesam Jabali <wesamjabali@icloud.com>
2021-09-26 00:15:13 -0500

    add alias add and remove commands

---
+++ src/commands/alias/add/add.command.ts
@@ -0,0 +1,74 @@
+import { CommandInteraction } from "discord.js";
+import {
+  CommandOption,
+  CommandOptionPermission,
+  ICommand,
+} from "@/commands/command.interface";
+
+import { getGuildConfig } from "@/config/guilds.config";
+import { normalizeCourseCode } from "@/helpers/normalize-course-code.helper";
+import { prisma } from "@/prisma/prisma.service";
+
+export class AddCourseAliasCommand implements ICommand {
+  name = "add";
+  description = "Add an alias to a course";
+  default_permission = false;
+  permissions: CommandOptionPermission[] = [
+    { type: "CourseManager", permission: true },
+  ];
+
+  options: CommandOption[] = [
+    {
+      type: "String",
+      name: "course",
+      description: "Name of the course to add the alias to",
+      required: true,
+    },
+    {
+      type: "String",
+      name: "alias",
+      description: "The new alias",
+      required: true,
+    },
+    {
+      type: "String",
+      name: "quarter",
+      description: "Name of the quarter this should take effect in",
+      required: false,
+    },
+  ];
+
+  async execute(i: CommandInteraction) {
+    const guildConfig = getGuildConfig(i.guildId);
+    let courseName = normalizeCourseCode(
+      i.options.getString("course", true)
+    ).courseName;
+    let newAlias = normalizeCourseCode(i.options.getString("alias", true));
+    const quarterName = i.options.getString("quarter", false);
+    let dbQuarter = guildConfig?.currentQuarter;
+    if (quarterName) {
+      dbQuarter = guildConfig?.quarters.find((q) => q.name === quarterName);
+    }
+    if (!dbQuarter) {
+      return i.reply(`Quarter ${quarterName} doesn't exist.`);
+    }
+    if (!newAlias.courseCodeNumber || !newAlias.courseCodePrefix) {
+      return i.reply(`Invalid alias, please use this template: **CSC300-401**`);
+    }
+
+    const dbCourseIndex = guildConfig?.courses.findIndex(
+      (c) => c.aliases.includes(courseName) && c.quarterId === dbQuarter?.id
+    );
+
+    if (dbCourseIndex === -1 || !dbCourseIndex) {
+      return i.reply(`${courseName} doesn't exist.`);
+    }
+
+    guildConfig?.courses[dbCourseIndex].aliases.push(newAlias.courseName);
+
+    await prisma.course.update({
+      where: { id: guildConfig?.courses[dbCourseIndex].id },
+      data: { aliases: guildConfig?.courses[dbCourseIndex].aliases },
+    });
+  }
+}
---
+++ src/commands/alias/remove/remove.command.ts
@@ -0,0 +1,68 @@
+import { CommandInteraction } from "discord.js";
+import {
+  CommandOption,
+  CommandOptionPermission,
+  ICommand,
+} from "@/commands/command.interface";
+
+import { getGuildConfig } from "@/config/guilds.config";
+import { normalizeCourseCode } from "@/helpers/normalize-course-code.helper";
+import { prisma } from "@/prisma/prisma.service";
+
+export class RemoveCourseAliasCommand implements ICommand {
+  name = "remove";
+  description = "Remove an alias from a course";
+  default_permission = false;
+  permissions: CommandOptionPermission[] = [
+    { type: "CourseManager", permission: true },
+  ];
+
+  options: CommandOption[] = [
+    {
+      type: "String",
+      name: "alias",
+      description: "The alias to remove",
+      required: true,
+    },
+    {
+      type: "String",
+      name: "quarter",
+      description: "Name of the quarter this should take effect in",
+      required: true,
+    },
+  ];
+
+  async execute(i: CommandInteraction) {
+    const guildConfig = getGuildConfig(i.guildId);
+    if (!guildConfig) return;
+
+    let aliasToRemove = normalizeCourseCode(
+      i.options.getString("alias", true)
+    ).courseName;
+    const quarterName = i.options.getString("quarter", false);
+    let dbQuarter = guildConfig.currentQuarter ?? undefined;
+    if (quarterName) {
+      dbQuarter = guildConfig.quarters.find((q) => q.name === quarterName);
+    }
+    if (!dbQuarter) {
+      return i.reply(`Quarter ${quarterName} doesn't exist.`);
+    }
+
+    const dbCourseIndex = guildConfig?.courses.findIndex(
+      (c) => c.aliases.includes(aliasToRemove) && c.quarterId === dbQuarter?.id
+    );
+
+    if (dbCourseIndex === -1 || !dbCourseIndex) {
+      return i.reply(`${aliasToRemove} doesn't exist.`);
+    }
+
+    guildConfig.courses[dbCourseIndex].aliases = guildConfig?.courses[
+      dbCourseIndex
+    ].aliases.filter((a) => a !== aliasToRemove);
+
+    await prisma.course.update({
+      where: { id: guildConfig?.courses[dbCourseIndex].id },
+      data: { aliases: guildConfig?.courses[dbCourseIndex].aliases },
+    });
+  }
+}
