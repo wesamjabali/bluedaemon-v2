94d3b4375ac5818f0687d36da62e24bf3f8d2685
Wesam Jabali <wesamjabali@icloud.com>
2021-10-03 21:40:57 -0500

    Reimplement ping command

--- src/commands/ping/ping.command.ts
+++ src/commands/ping/ping.command.ts
@@ -1,7 +1,5 @@
-import { CommandInteraction, Guild } from "discord.js";
+import { CommandInteraction } from "discord.js";
 import { ICommand } from "@/commands/command.interface";
-import { getGuildConfig } from "@/config/guilds.config";
-import { resetCacheForGuild } from "@/helpers/reset-cache-for-guild.helper";
 
 export class PingCommand implements ICommand {
   name = "ping";
@@ -10,9 +8,7 @@ export class PingCommand implements ICommand {
 
   public async execute(interaction: CommandInteraction): Promise<void> {
     await interaction.reply("Pong!");
-    const guildConfig = getGuildConfig(interaction.guildId);
-    console.log(guildConfig?.qotds);
-    await resetCacheForGuild(interaction.guildId as string, "qotds")
+
     const replyTime = new Date();
 
     await interaction.editReply({
