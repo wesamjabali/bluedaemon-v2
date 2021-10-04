1efc75eea1eaacb9bf275f37bb3fbc955affae26
Wesam Jabali <wesamjabali@icloud.com>
2021-10-03 21:12:24 -0500

    temp qotd console

--- src/commands/ping/ping.command.ts
+++ src/commands/ping/ping.command.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction } from "discord.js";
 import { ICommand } from "@/commands/command.interface";
+import { getGuildConfig } from "@/config/guilds.config";
 
 export class PingCommand implements ICommand {
   name = "ping";
@@ -8,6 +9,8 @@ export class PingCommand implements ICommand {
 
   public async execute(interaction: CommandInteraction): Promise<void> {
     await interaction.reply("Pong!");
+    const guildConfig = getGuildConfig(interaction.guildId);
+    console.log(guildConfig?.qotds);
 
     const replyTime = new Date();
 
